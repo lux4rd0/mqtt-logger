af06011f33fc7e301e1e99bcde996e6e00b87896
Cougar <cougar@random.ee>
2022-11-27 01:03:07 +0200

    Make MQTT topics configurable via `MQTT_LOGGER_MQTT_TOPICS`

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-mosquitto_sub -h $MQTT_LOGGER_MQTT_HOSTNAME -p $MQTT_LOGGER_MQTT_PORT -u $MQTT_LOGGER_MQTT_USERNAME -P $MQTT_LOGGER_MQTT_PASSWORD -F %j -t '+/debug' -t 'homeassistant/#' | promtail --stdin --config.expand-env=true --config.file=/mqtt-logger/config.yaml
-
+for item in $MQTT_LOGGER_MQTT_TOPICS; do mqtt_topics="${mqtt_topics} -t $item"; done
+mosquitto_sub -h $MQTT_LOGGER_MQTT_HOSTNAME -p $MQTT_LOGGER_MQTT_PORT -u $MQTT_LOGGER_MQTT_USERNAME -P $MQTT_LOGGER_MQTT_PASSWORD -F %j $mqtt_topics | promtail --stdin --config.expand-env=true --config.file=/mqtt-logger/config.yaml
